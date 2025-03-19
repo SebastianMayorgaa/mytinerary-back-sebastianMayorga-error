6e18ec8465cae17fae4f62dc71214ff17c105df4
SebastianMayorgaa <jsebastianrmy@gmail.com>
2025-03-19 10:59:56 -0600

    Agregue carpetas

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import 'dotenv/config.js'
+
+const server =express()
+
+const PORT = process.env.PORT
+
+const ready = () => console.log("Server ready in port:"+PORT);
+
+server.listen(PORT,ready) 
